8c52ea0b4e35b836fa66e911a650d19c31067fa1
flaviobarros <flaviomargarito@gmail.com>
2015-12-30 01:11:31 -0700

    Using template_tag inside a template

--- newsletter/forms.py
+++ newsletter/forms.py
@@ -6,3 +6,12 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = SignUp
         fields = ['full_name','email']
+
+    def clean_email(self):
+
+        email = self.cleaned_data.get('email')
+
+        if not "edu" in email:
+            raise forms.ValidationError("Please use a valid .EDU email address")
+        
+        return email
--- newsletter/views.py
+++ newsletter/views.py
@@ -3,4 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 
-    return render(request, "home.html", {})
+    title = 'Meu titulo'
+    context = {
+            "template_title": title,
+            
+            }
+
+    return render(request, "home.html", context)
